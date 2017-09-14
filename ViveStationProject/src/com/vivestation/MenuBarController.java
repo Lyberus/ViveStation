30ea2be21e5b185d822efedbd3ea56b0fc069f0b
DEEPRAIN <DEEPRAIN@X56KS>
2017-09-14 11:57:37 +0900

    [변경사항] 음

--- ViveStationProject/src/com/vivestation/MenuBarController.java
+++ ViveStationProject/src/com/vivestation/MenuBarController.java
@@ -1,14 +1,33 @@
 package com.vivestation;
 
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class MenuBarController {
 
 	public Initializable root;
 	
+	@FXML
+	private ImageView IV_Logo;
+	
 	public MenuBarController(Initializable root) {
 		this.root = root;
+	}
+	
+	public void initializable(URL location, ResourceBundle resources) {
+		IV_Logo.setOnMouseClicked(event -> FinishProgram(event));
+	}
+	
+	public void FinishProgram(MouseEvent event) {
+		Platform.exit();
 	}
 	
 }
