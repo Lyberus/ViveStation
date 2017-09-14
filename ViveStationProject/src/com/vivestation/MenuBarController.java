aff8378bd67c24dc7b197998af815f30371a3307
DEEPRAIN <DEEPRAIN@X56KS>
2017-09-14 15:53:40 +0900

    [변경사항] 좌상단 로고 클릭하면 프로그램 종료

--- ViveStationProject/src/com/vivestation/MenuBarController.java
+++ ViveStationProject/src/com/vivestation/MenuBarController.java
@@ -1,33 +1,17 @@
 package com.vivestation;
 
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.application.Platform;
-import javafx.event.Event;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-
 public class MenuBarController {
 
-	public Initializable root;
+	public RootController root;
 	
-	@FXML
-	private ImageView IV_Logo;
-	
-	public MenuBarController(Initializable root) {
+	public MenuBarController(RootController root) {
 		this.root = root;
+		init();
 	}
 	
-	public void initializable(URL location, ResourceBundle resources) {
-		IV_Logo.setOnMouseClicked(event -> FinishProgram(event));
-	}
-	
-	public void FinishProgram(MouseEvent event) {
-		Platform.exit();
+	public void init() {
+		root.IV_Logo.setOnMouseClicked(event -> root.finishProgram());
 	}
 	
 }
--- ViveStationProject/src/com/vivestation/RootController.java
+++ ViveStationProject/src/com/vivestation/RootController.java
@@ -3,14 +3,26 @@ package com.vivestation;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 public class RootController implements Initializable {
 
+	@FXML
+	public ImageView IV_Logo;
+	
+	public MenuBarController contMenuBar;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+		contMenuBar = new MenuBarController(this);
+	}
+	
+	public void finishProgram() {
+		Platform.exit();
 	}
 
 }
